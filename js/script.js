de53966d68459905b9c7147b12daed235485ba32
Wasley <88601440+wasleyfps@users.noreply.github.com>
2023-09-11 10:32:06 -0300

    update

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const calculator = document.getElementsByClassName('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+
+keys.addEventListener('click', e => {
+ if (e.target.matches('button')) {
+   // Fazer algo
+ }
+})
+
+const key = e.target
+const action = key.dataset.action
+if (!action) {
+    console.log('number key!')
+  }
+
+if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) {
+    console.log('operator key!')
+  }
+  if (action === 'decimal') {
+    console.log('decimal key!')
+  }
+  
+  if (action === 'clear') {
+    console.log('clear key!')
+  }
+  
+  if (action === 'calculate') {
+    console.log('equal key!')
+  }
